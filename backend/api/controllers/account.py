5d453a32f97bf04487340efc4313d8655316f5e0
danya <daniil.kuvaldin@elastoo.com>
2020-07-10 01:32:10 +0300

    Add account registration/login/verification

--- backend/api/controllers/account.py
+++ backend/api/controllers/account.py
@@ -11,8 +11,8 @@ from schemas.user import (
     User,
     UserLoginResponse,
     UserCreationSafe,
-    UserUpdateSafe,
     UserChangePassword,
+    UserVerify
 )
 
 __all__ = ["router"]
@@ -20,25 +20,24 @@ __all__ = ["router"]
 router = APIRouter()
 
 
-@router.post("/login/", response_model=UserLoginResponse)
+@router.post("/login/", response_model=UserLoginResponse, response_model_exclude={"_id"})
 async def account_login(data: UserLogin = Body(...)):
     return await UserCRUD.authenticate(data.email, data.password)
 
 
-@router.post("/signup/", response_model=UserLoginResponse)
+@router.post("/signup/")
 async def account_signup(data: UserCreationSafe = Body(...)):
     return await UserCRUD.create_safe(data)
+
+
+@router.post("/verify/", response_model=UserLoginResponse, response_model_exclude={"_id"})
+async def account_verify(data: UserVerify = Body(...)):
+    return await UserCRUD.verify(data.email, data.verification_code)
 
 
 @router.get("/user/", response_model=User, response_model_exclude={"_id"})
 async def account_get_user(user: User = Depends(get_user)):
     return user
-
-
-@router.put("/user/")
-async def account_update_user(user: User = Depends(get_user), payload: UserUpdateSafe = Body(...)):
-    resp = await UserCRUD.update_safe(user, payload) if payload.dict(exclude_unset=True) else {}
-    return resp
 
 
 @router.post("/change_password/")

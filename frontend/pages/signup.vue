0a022124537c4c25b34811dba62ec74eddd7ca79
Sarmat Khachirov <skhachirov12@gmail.com>
2020-07-23 18:24:23 +0300

    wip

--- frontend/pages/signup.vue
+++ frontend/pages/signup.vue
@@ -1,72 +1,87 @@
 <template lang="pug">
-  form.signup
-    header.signup__header
-      h1.signup__title Регистрация
-      nuxt-link.signup__subtitle(to="/login") Вход
-    main.signup__inputs
-      Input(v-model="email" placeholder="Имя пользователя" icon="user" type="text")
-      Input(placeholder="Эл. почта" icon="email" type="email")
-      Input(placeholder="Пароль" icon="password" type="password")
-    div.signup__action
-      Button(green) Зарегистрироваться
-    p.signup__terms Нажимая “зарегистрироваться”, я принимаю 
-      nuxt-link.signup__link(to="/") политику в отношении персональных данных 
-      span и 
-      nuxt-link.signup__link(to="/")  пользовательское соглашение 
+	form.signup
+		header.signup__header
+			h1.signup__title Регистрация
+			nuxt-link.signup__subtitle(to="/login") Вход
+		main.signup__inputs
+			Input(v-model="registerForm.userName" placeholder="Имя пользователя" icon="user" type="text")
+			Input(v-model="registerForm.email" placeholder="Эл. почта" icon="email" type="email")
+			Input(v-model="registerForm.password" placeholder="Пароль" icon="password" type="password")
+			Input(v-model="registerForm.passwordRepeat" placeholder="Подтверждение пароля" icon="password" type="password")
+		div.signup__action
+			Button(green @click.native="signUp") Зарегистрироваться
+		p.signup__terms Нажимая “зарегистрироваться”, я принимаю 
+			nuxt-link.signup__link(to="/") политику в отношении персональных данных 
+			span и 
+			nuxt-link.signup__link(to="/") пользовательское соглашение 
 </template>
 
 <script>
 import Input from "~/components/app/Input";
 import Button from "~/components/app/Button";
 export default {
-  components: { Input, Button },
-  data() {
-    return {
-      email: ''
-    }
-  },
+	components: { Input, Button },
+	data() {
+		return {
+			registerForm: {
+        userName: '',
+        email: '',
+        password: '',
+        passwordRepeat: ''
+			}
+		}
+	},
+	methods: {
+		async signUp() {
+      console.log(this.registerForm)
+			let resp = await this.$store.dispatch("signUp", this.registerForm);
+      if (resp) {
+        this.$router.push('/');
+      }
+
+		}
+	}
 };
 </script>
 
 <style lang="scss">
 .signup {
-  width: 390px;
-  margin: auto;
-  padding-top: 100px;
+	width: 390px;
+	margin: auto;
+	padding-top: 100px;
 
-  &__header {
-    margin-bottom: 60px;
-  }
+	&__header {
+		margin-bottom: 60px;
+	}
 
-  &__title {
-    margin-bottom: 8px;
-  }
+	&__title {
+		margin-bottom: 8px;
+	}
 
-  &__subtitle {
-    @include montserrat;
-    @include reset-link;
-    text-decoration-line: underline;
-    opacity: 0.5;
-  }
+	&__subtitle {
+		@include montserrat;
+		@include reset-link;
+		text-decoration-line: underline;
+		opacity: 0.5;
+	}
 
-  &__inputs {
-    height: 175px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-  }
+	&__inputs {
+		& > * {
+			margin-bottom: 20px;
+		}
+	}
 
-  &__action {
-    margin-top: 50px;
-    margin-bottom: 20px;
-  }
+	&__action {
+		margin-top: 50px;
+		margin-bottom: 20px;
+	}
 
-  &__terms {
-  }
+	&__terms {
+	}
 
-  &__link {
-    @include reset-link;
-    color: $orange !important;
-  }
+	&__link {
+		@include reset-link;
+		color: $orange !important;
+	}
 }
 </style>

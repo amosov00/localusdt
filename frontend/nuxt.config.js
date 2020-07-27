5686424080c9301df4228b5a9be52a3a4187b76a
Sarmat Khachirov <skhachirov12@gmail.com>
2020-07-27 17:54:13 +0300

    install vee-validate

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,60 +1,61 @@
 export default {
-  mode: "spa",
+  mode: 'spa',
 
   head: {
-    title: "LocalUSDT",
+    title: 'LocalUSDT',
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
-        rel: "stylesheet",
+        rel: 'stylesheet',
         href:
-          "https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;600;700&display=swap",
+          'https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;600;700&display=swap'
       },
       {
-        rel: "stylesheet",
+        rel: 'stylesheet',
         href:
-          "https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700&display=swap",
-      },
-    ],
+          'https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700&display=swap'
+      }
+    ]
   },
 
-  loading: { color: "#48B190" },
+  loading: { color: '#48B190' },
 
-  css: ["~/assets/scss/global.scss"],
+  css: ['~/assets/scss/global.scss'],
 
   plugins: [
-    "~/plugins/auth.js",
-    "~/plugins/axios.js",
-    "~/plugins/toast.js"
+    '~/plugins/auth.js',
+    '~/plugins/axios.js',
+    '~/plugins/toast.js',
+    '~/plugins/vee-validate.js'
   ],
   modules: [
-    "@nuxtjs/axios",
-    "@nuxtjs/dotenv",
-    "@nuxtjs/sentry",
-    "cookie-universal-nuxt",
+    '@nuxtjs/axios',
+    '@nuxtjs/dotenv',
+    '@nuxtjs/sentry',
+    'cookie-universal-nuxt',
     '@nuxtjs/style-resources'
   ],
   styleResources: {
-    scss: ["~/assets/scss/variables.scss", "~/assets/scss/mixins.scss"],
+    scss: ['~/assets/scss/variables.scss', '~/assets/scss/mixins.scss']
   },
-  buildModules: ["@nuxtjs/dotenv"],
+  buildModules: ['@nuxtjs/dotenv'],
   dotenv: !process.env.ENV
     ? {
-        filename: ".env.local",
+        filename: '.env.local'
       }
     : {},
   sentry: {
     initialize: true,
     config: {
-      environment: process.env.ENV,
-    },
+      environment: process.env.ENV
+    }
   },
   build: {
-    extend(config, ctx) {},
-  },
-};
+    extend(config, ctx) {}
+  }
+}

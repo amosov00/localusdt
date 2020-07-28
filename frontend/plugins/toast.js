1d44d639484d6906a52a082b55c550db35b75424
Sarmat <sarmat.khachirov@elastoo.com>
2020-07-28 15:51:41 +0300

    improve toast

--- frontend/plugins/toast.js
+++ frontend/plugins/toast.js
@@ -1,7 +1,7 @@
 export default ({ app, store }, inject) => {
   inject('toast', {
-    showMessage ({ content = '', color = '' }) {
-      store.commit('toast/showMessage', { content, color })
+    showMessage ({ content = '', color = '', red, green }) {
+      store.commit('toast/showMessage', { content, color, red, green })
     }
   })
 }

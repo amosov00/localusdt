18f5186b6eef2c786857d4f0c06f446d1d6827e0
Sarmat <sarmat.khachirov@elastoo.com>
2020-07-30 18:56:48 +0300

    ads store

--- frontend/store/ads.js
+++ frontend/store/ads.js
@@ -1,22 +1,41 @@
 export const state = () => ({
-  ads: []
+  ads: [],
+  adById: null
 })
 
 export const getters = {
-  ads: s => s.ads
+  ads: s => s.ads,
+  adById: s => s.adById,
+  buyAds: s => s.ads.filter(ad => ad.type === 1),
+  sellAds: s => s.ads.filter(ad => ad.type === 2)
 }
 
 export const mutations = {
-  setAds: (state, payload) => (state.ads = payload)
+  setAds: (state, payload) => (state.ads = payload),
+  setAdById: (state, payload) => (state.adById = payload)
 }
 
 export const actions = {
-  async fetchAds({ commit }, limit = 10) {
-    const { data } = await this.$axios.get('/ads', {
-      params: {
-        limit
-      }
-    })
-    console.log(data)
+  async fetchAds({ commit }) {
+    const { data } = await this.$axios.get('/ads/')
+    commit('setAds', data)
+  },
+  async fetchAdById({ commit }, id) {
+    const { data } = await this.$axios.get(`/ads/${id}`)
+    commit('setAdById', data)
+  },
+  async createAd({}, adForm) {
+    return await this.$axios
+      .post('/ads/', { adForm })
+      .then(() => {
+        this.$toast.showMessage({
+          content: 'Ваше объявление успешно размещено!',
+          green: true
+        })
+      })
+      .catch(error => {
+        console.log(error.response)
+        this.$toast.showMessage({ content: error.response.data[0].message, red: true })
+      })
   }
 }

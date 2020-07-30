7c2f60adc89207ddb2e31d7f2b067e4af331a3b6
Sarmat <sarmat.khachirov@elastoo.com>
2020-07-30 18:55:35 +0300

    improve

--- frontend/components/app/Input.vue
+++ frontend/components/app/Input.vue
@@ -12,148 +12,149 @@
 /* eslint-disable */
 import InlineSvg from 'vue-inline-svg'
 export default {
-	components: { InlineSvg },
-	props: {
-		type: {
-			type: String,
-			default: 'text'
-		},
-		placeholder: {
-			type: String,
-			default: ''
-		},
-		width: {
-			type: Number
-		},
-		icon: {
-			type: String,
-			required: false
-		},
-		endIcon: {
-			type: String,
-			required: false
-		},
-		header: {
-			type: String,
-			default: ''
-		}
-	},
-	data() {
-		return {
-			showPassword: false,
-			value: ''
-		}
-	},
-	computed: {
-		passwordIcon() {
-			if (this.showPassword) {
-				return 'eye'
-			} else {
-				return 'eye-off'
-			}
-		},
-		showPasswordField() {
-			if (this.type === 'password' && this.showPassword) {
-				return 'text'
-			} else {
-				return this.type
-			}
-		},
-		inputIcon() {
-			if (this.icon) {
-				return true
-			} else {
-				return false
-			}
-		},
-		inputEndIcon() {
-			if (this.endIcon) {
-				return true
-			} else {
-				return false
-			}
-		},
-		inputHeader() {
-			if (this.header) {
-				return true
-			} else {
-				return false
-			}
-		}
-	},
-	methods: {
-		toggleShowPassword() {
-			this.showPassword = !this.showPassword
-		},
-		updateValue (value) {
-			this.$emit('input', value)
-		},
-	}
+  components: { InlineSvg },
+  props: {
+    type: {
+      type: String,
+      default: 'text'
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    width: {
+      type: Number
+    },
+    icon: {
+      type: String,
+      required: false
+    },
+    endIcon: {
+      type: String,
+      required: false
+    },
+    header: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      showPassword: false,
+      value: ''
+    }
+  },
+  computed: {
+    passwordIcon() {
+      if (this.showPassword) {
+        return 'eye'
+      } else {
+        return 'eye-off'
+      }
+    },
+    showPasswordField() {
+      if (this.type === 'password' && this.showPassword) {
+        return 'text'
+      } else {
+        return this.type
+      }
+    },
+    inputIcon() {
+      if (this.icon) {
+        return true
+      } else {
+        return false
+      }
+    },
+    inputEndIcon() {
+      if (this.endIcon) {
+        return true
+      } else {
+        return false
+      }
+    },
+    inputHeader() {
+      if (this.header) {
+        return true
+      } else {
+        return false
+      }
+    }
+  },
+  methods: {
+    toggleShowPassword() {
+      this.showPassword = !this.showPassword
+    },
+    updateValue(value) {
+      this.$emit('input', value)
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 .input {
-	font-family: 'Montserrat';
-	font-style: normal;
-	font-weight: 500;
-	font-size: 12px;
-	position: relative;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 12px;
+  position: relative;
 
-	&__header {
-		opacity: 0.7;
-		margin-left: 2px;
-		margin-bottom: 5px;
-	}
+  &__header {
+    font-size: 12px;
+    opacity: 0.7;
+    margin-left: 2px;
+    margin-bottom: 5px;
+  }
 
-	&__field {
-		height: 45px;
-		width: 100%;
-		opacity: 0.8;
-		background-color: $grey-light;
-		border: 1px solid transparent;
-		outline: none;
-		border-radius: $border-radius;
-		padding: 20px 45px;
-		transition: $interaction-transition;
+  &__field {
+    height: 45px;
+    width: 100%;
+    opacity: 0.8;
+    background-color: $grey-light;
+    border: 1px solid transparent;
+    outline: none;
+    border-radius: $border-radius;
+    padding: 20px 45px;
+    transition: $interaction-transition;
 
-		&:focus {
-			box-shadow: $box-shadow;
-			border: 1px solid #c8c8c8;
-			caret-color: $orange;
-		}
-	}
+    &:focus {
+      box-shadow: $box-shadow;
+      border: 1px solid #c8c8c8;
+      caret-color: $orange;
+    }
+  }
 
-	&__icon {
-		transform: translate(-50%, -50%);
-		position: absolute;
-		top: 50%;
-		left: 25px;
+  &__icon {
+    transform: translate(-50%, -50%);
+    position: absolute;
+    top: 50%;
+    left: 25px;
 
-		&--end {
-			transform: translate(-50%, -50%);
-			position: absolute;
-			top: 70%;
-			right: 10px;
-			font-size: 16px;
-			word-wrap: break-word;
-			text-transform: uppercase;
-			opacity: 0.5;
-		}
+    &--end {
+      transform: translate(-50%, -50%);
+      position: absolute;
+      top: 70%;
+      right: 10px;
+      font-size: 16px;
+      word-wrap: break-word;
+      text-transform: uppercase;
+      opacity: 0.5;
+    }
 
-		&--eye {
-			cursor: pointer;
-			transform: translate(-50%, -50%);
-			position: absolute;
-			top: 50%;
-			right: 20px;
-			opacity: 0.5;
-		}
-	}
+    &--eye {
+      cursor: pointer;
+      transform: translate(-50%, -50%);
+      position: absolute;
+      top: 50%;
+      right: 20px;
+      opacity: 0.5;
+    }
+  }
 
-	&__error {
-		color: $red;
-		// margin: 2px;
-	}
+  &__error {
+    color: $red;
+    // margin: 2px;
+  }
 }
 </style>
--- frontend/components/app/Textarea.vue
+++ frontend/components/app/Textarea.vue
@@ -1,5 +1,7 @@
 <template lang="pug">
-  textarea.textarea(:placeholder="placeholder")
+  div.textarea-box
+    p.textarea-box__header {{header}}
+    textarea.textarea(@input="updateValue($event.target.value)" :value="value" :placeholder="placeholder")
 </template>
 
 <script>
@@ -9,32 +11,55 @@ export default {
       type: String,
       default: '',
       require: false
+    },
+    header: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      value: ''
+    }
+  },
+  methods: {
+    updateValue(value) {
+      this.$emit('input', value)
     }
   }
 }
 </script>
 
 <style lang="scss">
-.textarea {
-  font-family: 'Montserrat';
-  font-style: normal;
-  font-weight: 500;
-  font-size: 12px;
-  background-color: #f6f6f6;
-  opacity: 0.8;
-  background-color: $grey-light;
-  border: 1px solid transparent;
-  outline: none;
-  border-radius: $border-radius;
-  padding: 20px;
-  transition: $interaction-transition;
-  width: 475px;
-  height: 175px;
+.textarea-box {
+  &__header {
+    font-size: 12px;
+    opacity: 0.7;
+    margin-left: 2px;
+    margin-bottom: 5px;
+  }
 
-  &:focus {
-    box-shadow: $box-shadow;
-    border: 1px solid #c8c8c8;
-    caret-color: $orange;
+  .textarea {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 12px;
+    background-color: #f6f6f6;
+    opacity: 0.8;
+    background-color: $grey-light;
+    border: 1px solid transparent;
+    outline: none;
+    border-radius: $border-radius;
+    padding: 20px;
+    transition: $interaction-transition;
+    width: 475px;
+    height: 175px;
+
+    &:focus {
+      box-shadow: $box-shadow;
+      border: 1px solid #c8c8c8;
+      caret-color: $orange;
+    }
   }
 }
 </style>

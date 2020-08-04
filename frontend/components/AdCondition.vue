189967c28adf0f22f254079492bc82ada04b8f9c
Sarmat <sarmat.khachirov@elastoo.com>
2020-08-04 15:38:58 +0300

    split ads page for different components

---
+++ frontend/components/AdCondition.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="ad-condition">
+    <h2 class="ad-condition__title">Условия сделки</h2>
+    <div class="ad-condition__box">
+      <p class="opacity-50">{{ adCondition }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    adCondition: String
+  }
+}
+</script>
+
+<style lang="scss">
+.ad-condition {
+  min-width: 350px;
+  max-width: 500px;
+  width: 100%;
+
+  &__title {
+    font-weight: 500;
+    font-size: 20px;
+  }
+
+  &__box {
+    margin-top: 30px;
+    background: rgba(72, 177, 144, 0.05);
+    border-radius: $border-radius;
+    padding: 30px 15px;
+  }
+}
+</style>
---
+++ frontend/components/AdDetails.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="ad-details">
+    <div class="ad-details__row fz-20">
+      <p class="ad-details__cell opacity-50">Цена</p>
+      <p class="ad-details__cell">{{ commaSplitting(adDetails.price || adDetails.amount_rub) }} ₽/USDT</p>
+    </div>
+    <div class="ad-details__row">
+      <p class="ad-details__cell opacity-50" v-if="adDetails.type || adDetails.ads_type === 1">Продавец:</p>
+      <p class="ad-details__cell opacity-50" v-else-if="adDetails.type || adDetails.ads_type === 2">Покупатель:</p>
+      <p class="ad-details__cell">{{ adDetails.username || adDetails.buyer_username }}</p>
+    </div>
+    <div class="ad-details__row">
+      <p class="ad-details__cell opacity-50">Способ оплаты:</p>
+      <p class="ad-details__cell">Банковский перевод: Сбербанк</p>
+    </div>
+    <hr>
+    <div class="ad-details__row mt-10">
+      <p class="ad-details__cell opacity-50">Ограничения по сделке:</p>
+      <p class="ad-details__cell" v-if="adDetails.bot_limit && adDetails.top_limit"><span>{{spaceSplitting(adDetails.bot_limit)}}</span> — <span>{{spaceSplitting(adDetails.top_limit)}} ₽</span></p>
+    </div>
+    <div class="ad-details__row mt-10">
+      <p class="ad-details__cell opacity-50">Местоположение:</p>
+      <p class="ad-details__cell">Российская Федерация</p>
+    </div>
+    <div class="ad-details__row mt-10">
+      <p class="ad-details__cell opacity-50">Окно оплаты:</p>
+      <p class="ad-details__cell">1 час 30 минут</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import formatCurreny from '~/mixins/formatCurrency'
+export default {
+  props: {
+    adDetails: Object
+  },
+  mixins: [formatCurreny]
+}
+</script>
+
+<style lang="scss">
+.ad-details {
+  width: 450px;
+  font-size: 14px;
+
+  &__row {
+    display: flex;
+    justify-content: space-between;
+
+    & > * {
+      margin-bottom: 15px;
+    }
+  }
+
+  &__cell {
+    flex: 1;
+  }
+}
+</style>

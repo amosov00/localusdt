bef89bf132d33fc1befdd031d2b4e95485b54cdf
Sarmat <sarmat.khachirov@elastoo.com>
2020-08-19 15:59:07 +0300

    fix sorting

--- frontend/store/invoice.js
+++ frontend/store/invoice.js
@@ -10,7 +10,7 @@ export const getters = {
 
 export const mutations = {
   setInvoices: (state, payload) => {
-    state.invoices = payload.sort((a, b) => b.created_at - a.created_at)
+    state.invoices = payload.reverse()
   },
   setInvoiceById: (state, payload) => {
     state.invoiceById = payload

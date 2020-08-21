6eeb04615c1c2a6d477275f3905692cfb2e37915
Sarmat <sarmat.khachirov@elastoo.com>
2020-08-21 17:36:44 +0300

    add format date mixin

--- frontend/mixins/formatDate.js
+++ frontend/mixins/formatDate.js
@@ -6,6 +6,11 @@ export default {
       return moment(timestamp)
         .utc()
         .format("DD.MM.YYYY HH:mm");
+      },
+      regularDate(timestamp) {
+        return moment(timestamp)
+          .utc()
+          .format("DD.MM.YYYY");
     }
   }
 };

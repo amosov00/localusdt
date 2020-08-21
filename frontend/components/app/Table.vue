baa19ed66335f302fd6b8912ab36131e734cbad7
Sarmat <sarmat.khachirov@elastoo.com>
2020-08-21 13:42:55 +0300

    add page walking

--- frontend/components/app/Table.vue
+++ frontend/components/app/Table.vue
@@ -23,16 +23,17 @@
               Button(rounded outlined green) {{buttonName}}
     div.pagination(v-if="pagination")
       div.pagination__controller
-        span.pagination__arrow-button(@click="prevPage")
+        button.pagination__arrow-button(@click="prevPage")
           InlineSvg.pagination__arrow-icon(:src="require('~/assets/icons/arrow-left.svg')")
-        span.pagination__arrow-button(@click="nextPage")
+        p
+          button.pagination__page-button(v-for="page in pagesCount" :key="page" @click="goToPage(page)" :class="{'orange': currentPage === page}" ) {{page}}
+        button.pagination__arrow-button(@click="nextPage")
           InlineSvg.pagination__arrow-icon(:src="require('~/assets/icons/arrow-right.svg')")
       div.pagination__quantity
-        span(:class="{'orange': contentPerPage === 15}" @click="contentPerPage = 15" ) 15
-        span(:class="{'orange': contentPerPage === 25}" @click="contentPerPage = 25" ) 25
-        span(:class="{'orange': contentPerPage === 50}" @click="contentPerPage = 50" ) 50
-        span(:class="{'orange': contentPerPage === 100}" @click="contentPerPage = 100" ) 100
-        span(:class="{'orange': contentPerPage === 200}" @click="contentPerPage = 200" ) 200
+        button.pagination__quantity-button(:class="{'black': contentPerPage === 15}" @click="contentPerPage = 15" ) 15
+        button.pagination__quantity-button(:class="{'black': contentPerPage === 25}" @click="contentPerPage = 25" ) 25
+        button.pagination__quantity-button(:class="{'black': contentPerPage === 50}" @click="contentPerPage = 50" ) 50
+        button.pagination__quantity-button(:class="{'black': contentPerPage === 100}" @click="contentPerPage = 100" ) 100
 </template>
 
 <script>
@@ -57,8 +58,7 @@ export default {
   data() {
     return {
       currentPage: 1,
-      contentPerPage: 15,
-      tableContent: []
+      contentPerPage: 15
     }
   },
   computed: {
@@ -66,33 +66,42 @@ export default {
       switch (this.pagination) {
         case true:
           return 'paginatedTableData'
-          break;
+          break
         case false:
           return 'tableData'
-          break;
-      
+          break
+
         default:
-          break;
+          break
       }
     },
     paginatedTableData() {
-      return this.tableData.slice(((this.currentPage * this.contentPerPage) - this.contentPerPage), (this.currentPage * this.contentPerPage))
+      return this.tableData.slice(
+        this.currentPage * this.contentPerPage - this.contentPerPage,
+        this.currentPage * this.contentPerPage
+      )
+    },
+    pagesCount() {
+      return Math.ceil(this.tableData.length / this.contentPerPage)
     }
   },
   methods: {
     prevPage() {
-      if(this.currentPage <= 1) {
+      if (this.currentPage <= 1) {
         return
       }
       this.currentPage -= 1
     },
     nextPage() {
-      if((this.paginatedTableData.length / this.contentPerPage) < 1) {
+      if (this.paginatedTableData.length / this.contentPerPage < 1) {
         return
       }
       this.currentPage += 1
+    },
+    goToPage(page) {
+      this.currentPage = page
     }
-  },
+  }
 }
 </script>
 
@@ -160,6 +169,7 @@ export default {
 }
 
 .pagination {
+  max-width: 1230px;
   width: 100%;
   height: 20px;
   display: flex;
@@ -175,18 +185,39 @@ export default {
   }
 
   &__arrow-button {
-    cursor: pointer;
+    @include reset-button;
+  }
+
+  &__page-button {
+    color: $grey-dark;
+    @include reset-button;
+
+    &:not(:last-child) {
+      margin-right: 30px;
+    }
   }
 
   &__quantity {
     display: inline-block;
-    width: 100px;
+    width: 115px;
     height: 20px;
-    span {
-      cursor: pointer;
+
       &:not(:last-child) {
         margin-right: 5px;
       }
+  }
+
+  &__quantity-button {
+    @include reset-button;
+    transition: $interaction-transition;
+    color: $grey-dark;
+
+    &:hover {
+      color: $black;
+    }
+
+    &:not(:last-child) {
+      margin-right: 10px;
     }
   }
 }

78881ee9ead9e8e62e7397ce466b1df3ef64a95b
dissmay <dissmay21@gmail.com>
2020-11-30 12:53:20 +0300

    fix socket

--- frontend/components/order/OrderForm.vue
+++ frontend/components/order/OrderForm.vue
@@ -51,6 +51,7 @@ export default {
   methods: {
     createInvoice() {
       if (this.invoiceForm.amount_usdt) {
+        console.log(this.invoiceForm);
         this.$store.dispatch('invoice/createInvoice', this.invoiceForm)
       } else {
         this.$toast.showMessage({ content: this.$t('orderForm.writeSum'), red: true })

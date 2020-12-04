e4a9c9d99069bd974f1cf0952f6d051e0d910aee
dissmay <dissmay21@gmail.com>
2020-12-04 15:41:47 +0300

    h

--- rundev-front.sh
+++ rundev-front.sh
@@ -1,8 +1,8 @@
 export DEBUG=true
 export ENV=local
 export NODE_ENV=development
-export API_URL=https://localusdt-dev.elastoo.com/api/
-export API_URL_BROWSER=https://localusdt-dev.elastoo.com/api/
+export API_URL=http://localusdt-dev.elastoo.com/api/
+export API_URL_BROWSER=http://localusdt-dev.elastoo.com/api/
 export API_WS_URL=wss://localusdt-dev.elastoo.com/api/
 
 cd frontend && npm run dev

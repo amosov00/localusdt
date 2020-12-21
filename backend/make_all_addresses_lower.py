37f9651d65cbce793e49641d467df818a898c3e0
danya <daniil.kuvaldin@elastoo.com>
2020-12-21 16:36:29 +0300

    add ethereum address filter to admin panel

--- backend/make_all_addresses_lower.py
+++ backend/make_all_addresses_lower.py
@@ -1,6 +1,7 @@
 import asyncio
 
 from database.crud.ethereum_wallet import EthereumWalletCRUD
+from database.crud.user import UserCRUD
 
 
 async def make_all_addresses_lower():
@@ -11,6 +12,14 @@ async def make_all_addresses_lower():
         },
             payload={"eth_address": address.get("eth_address").lower()}
         )
+    users = await UserCRUD.find_many({})
+    for user in users:
+        if user.get("eth_address"):
+            await UserCRUD.update_one(query={
+                "_id": user.get("_id")
+            },
+                payload={"eth_address": user.get("eth_address").lower()}
+            )
 
 
 if __name__ == '__main__':
